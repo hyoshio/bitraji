55d5cb62c9293ff733a5ef201e913dd9a5e67923
hyoshiza <hyossy938@gmail.com>
2018-09-20 23:53:12 +0900

    fix include

--- bitraji.user.js
+++ bitraji.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @include     https://lightning.bitflyer.*/trade
+// @include     https://lightning.bitflyer.*/trade*
 // @grant        none
 // @noframes
 // ==/UserScript==
