5f866ab37fba391835dafb6734062702df69a8cb
hyoshiza <hyossy938@gmail.com>
2018-09-20 23:59:00 +0900

    save

--- bitraji.user.js
+++ bitraji.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bitflyer
 // @namespace    test
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://lightning.bitflyer.jp/trade*
